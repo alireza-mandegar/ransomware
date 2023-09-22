7c5144ec7a55fdd9e460857f91ccb526fe818bd8
Alireza.Mandegar <57321058+alireza-mandegar@users.noreply.github.com>
2023-09-23 02:21:50 +0330

    key function

--- voldemort.py
+++ voldemort.py
@@ -4,3 +4,13 @@ from cryptography.fernet import Fernet
 E = "[-]"
 S = "[+]"
 I = "[*]"
+
+def key():
+    for file in os.listdir():
+        if file == "thekey.key":
+                print(f"{E} The key was generated!")
+                return
+    key = Fernet.generate_key()
+    with open("thekey.key", "wb") as thekey:
+        thekey.write(key)
+    print(f'{S} key generated!')
